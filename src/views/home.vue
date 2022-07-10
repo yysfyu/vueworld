fca303cf4017a68fb3c11b3c058e8beebdf4fb87
yysfyu <yysfyu@163.com>
2022-07-10 10:57:12 +0800

    2022-07-10：【变更说明】：倒计时组件封装使用

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,55 +1,51 @@
 <template>
     <div class="home">
-        <el-container>
-            <el-header height="50px" >Header</el-header>
-            <el-container>
-                <el-aside width="200px">
-                    <leftAside></leftAside>
-                </el-aside>
-                <el-container>
-                    <el-main>
-                        Main
-                    </el-main>
+        <h1>{{ msg }}</h1>
+        <slotchild v-slot="timeObj"  :end="countDown" :isMiniSecond="false">
+            <span>
+                {{timeObj.d}}天{{timeObj.hh}}小时{{timeObj.mm}}分钟{{timeObj.ss}}秒
+            </span>     
+        </slotchild>
 
-                    <el-footer>
-                        Footer
-                    </el-footer>
+        <slotchild v-slot="timeObj"  :time="60" :isMiniSecond="false">
+            <span>
+                {{timeObj.d}}天{{timeObj.hh}}小时{{timeObj.mm}}分钟{{timeObj.ss}}秒
+            </span>     
+        </slotchild>
 
-                </el-container>
-            </el-container>
-        </el-container>
+        <slotchild v-slot="timeObj"  :time="100 * 1000" :isMilliSecond="true">
+            <span>
+                {{timeObj.d}}天{{timeObj.hh}}小时{{timeObj.mm}}分钟{{timeObj.ss}}秒
+            </span>     
+        </slotchild>
     </div>
 </template>
 
 <script>
-import leftAside from './leftaside.vue'
+import slotchild from "../components/slot/timecountdown.vue"
 
 export default {
     name: 'home',
-    components: {leftAside},
+    components: {slotchild},
     data () {
         return {
-            
+            msg: "父组件",
+            countDown: "",
         }
-    }
+    },
+    methods: {
+        setCountDownData() {
+            this.countDown = (Date.parse(new Date()) + 48*60*60-50000) / 1000;
+            console.log(this.countDown)
+        }
+    },
+
+    created() {
+        this.setCountDownData();
+    },
 }
 </script>
 
 <style scoped>
-    .el-container {
-        height: 100%;
-    }
-    .el-header {
-        background-color: #b3c0d1;
-        color: #333;
-        line-height: 60px;
-    }
-    .el-aside {
-        display: block;
-        position: absolute;
-        left: 0;
-        top: 70px;
-        bottom: 0;
-        overflow-y: scroll;
-    }
+    
 </style>
